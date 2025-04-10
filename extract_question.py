5a30624d8048ab692d354e1c4caa854df7070b81
wonyzz <wonyz@duksung.ac.kr>
2025-04-11 01:19:17 +0900

    [add] 말뭉치 데이터 형태소 분석 결과 추가 #11

--- extract_question.py
+++ extract_question.py
@@ -1,25 +1,23 @@
 import json
-import uuid  # ⭐ UUID 생성 모듈 추가
 
-
-# 1. 원본 JSON 파일 열기
-with open("SFTdata.json", "r", encoding="utf-8") as f:
+# 원본 데이터 열기
+with open("data/SFTdata.json", "r", encoding="utf-8") as f:
     raw_data = json.load(f)
 
-# 2.필요한 항목만 추출
+# 필요한 항목만 추출
 processed_data = []
 
-for item in raw_data["data_info"]:
+for idx, item in enumerate(raw_data["data_info"]):
     question = item.get("question")
     
     if question:
         processed_data.append({
-            "data_id": str(uuid.uuid4()), 
+            "data_id": idx,  # 리스트 순서대로 번호 부여
             "question": question
         })
 
-# 3. 결과 저장
-with open("processed_data.json", "w", encoding="utf-8") as f:
+# 결과 저장
+with open("aiHubData.json", "w", encoding="utf-8") as f:
     json.dump(processed_data, f, ensure_ascii=False, indent=2)
 
-print(f"{len(processed_data)}개의 질문이 저장되었습니다!")
+print(f"{len(processed_data)}개의 질문이 저장되었습니다.")
---
+++ morpheme_analysis.py
@@ -0,0 +1,28 @@
+import json
+from konlpy.tag import Okt
+
+okt = Okt()
+
+# 데이터 불러오기
+with open('data/aiHubData.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# 분석 결과 저장용 리스트
+results = []
+
+for item in data:
+    question = item['question']
+    data_id = item['data_id']
+    pos_tags = okt.pos(question) # 전체 품사 태깅
+
+    results.append({
+        "data_id": data_id,
+        "question": question,
+        "pos_tags": pos_tags # 필터 없이 전체 저장
+    })
+
+# 결과를 JSON 파일로 저장
+with open('data/morpheme_results.json', 'w', encoding='utf-8') as f:
+    json.dump(results, f, ensure_ascii=False, indent=2)
+
+print("분석 완료! 형태소 분석 결과가 'morpheme_results.json'에 저장되었습니다.")
