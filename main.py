ef82ec77f20193a3c5c699518cd0ba9d587b9495
i-am-dahee <hxxly02@gmail.com>
2025-05-09 20:32:52 +0900

    [refactor] 포트 번호 수정 #13

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.register_blueprint(morpheme_bp)
 app.register_blueprint(homonym_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
