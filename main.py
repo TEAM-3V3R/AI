98e8c723f2567c25436d16d07e07900ace9d88f7
i-am-dahee <hxxly02@gmail.com>
2025-06-20 04:08:48 +0900

    [test] 주석 해제

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from prompt import prompt_bp
 from sam import sam_bp
-#from fastSAM import fastsam_bp
+from fastSAM import fastsam_bp
 from prompt_analyzer.analyzer import analyzer_bp
 from prompt_analyzer.categories import categories_bp
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # 각 기능 모듈의 Blueprint 등록
 app.register_blueprint(prompt_bp)
 app.register_blueprint(sam_bp)
-#app.register_blueprint(fastsam_bp)
+app.register_blueprint(fastsam_bp)
 app.register_blueprint(analyzer_bp)
 app.register_blueprint(categories_bp)
 
