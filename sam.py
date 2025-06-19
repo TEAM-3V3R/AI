1149c1fdbd37c66e314de3bfd0d9339ad2107846
76MINJI <126567535+76MINJI@users.noreply.github.com>
2025-06-19 19:48:03 +0900

    [feat] mask_generator 극한 경량화 설정

--- sam.py
+++ sam.py
@@ -166,7 +166,11 @@ def handle_sam():
         # 3. SAM 세그멘테이션 실행
         mask_generator = SamAutomaticMaskGenerator(
             model=sam,
-            points_per_side=8
+            points_per_side=4
+            pred_iou_thresh=0.95,
+            stability_score_thresh=0.95,
+            crop_n_layers=0,
+            min_mask_region_area=512
         )
         masks = mask_generator.generate(resized_image)
 
